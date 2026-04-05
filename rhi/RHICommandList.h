fb8f5ae32b0b583576ac45fb2f8a2bacf7134b13
mapmapbear <37301000+mapmapbear@users.noreply.github.com>
2026-04-05 22:48:37 +0800

    feat(rhi): add bindBindGroup to CommandList interface
    Add new bindBindGroup method to the CommandList abstract interface
    using BindGroupHandle instead of BindTableHandle. This is part of
    the transition to unified BindGroup abstraction for descriptor sets.
    
    Also adds BindGroupTag and BindGroupHandle to RHIHandles.h.

--- rhi/RHICommandList.h
+++ rhi/RHICommandList.h
@@ -96,6 +96,10 @@ public:
                              BindTableHandle   bindTable,
                              const uint32_t*   dynamicOffsets,
                              uint32_t          dynamicOffsetCount)                = 0;
+  virtual void bindBindGroup(uint32_t        slot,
+                             BindGroupHandle bindGroup,
+                             const uint32_t* dynamicOffsets,
+                             uint32_t        dynamicOffsetCount)                  = 0;
   virtual void bindVertexBuffers(uint32_t firstBinding, const BufferHandle* buffers, const uint64_t* offsets, uint32_t bufferCount) = 0;
   virtual void bindIndexBuffer(BufferHandle buffer, uint64_t offset, IndexFormat format) = 0;
   virtual void pushConstants(ShaderStage stages, uint32_t offset, uint32_t size, const void* data) = 0;
